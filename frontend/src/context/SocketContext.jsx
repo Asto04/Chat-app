39e7e9d51d1334342af520c8780735ffbfbc0fb6
Asto04 <7823802180am@gmail.com>
2024-04-28 16:29:40 +0530

    Socket completed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({children}) => {
     const {authUser} = useAuthContext();
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://khaitan-chatzone-w552.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 }
